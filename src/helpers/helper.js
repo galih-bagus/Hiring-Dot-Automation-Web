4e94388f727b133e3ec41a8afc142b38bd9893dc
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-07-23 10:48:29 +0700

    fix : create function openWebsite

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -3,20 +3,19 @@ const element = require("../helpers/element");
 
 driver = new Builder().forBrowser("chrome").build();
 
-async function login(user, password, first = true) {
-    if (first == true) {
-        vars = {};
-        let url = process.env.URL;
+async function openWebsite() {
+    let url = process.env.URL;
+    await driver.get(url);
+    await driver.manage().window().maximize();
+}
 
-        await driver.get(url);
-        await driver.manage().window().maximize();
-        await driver.sleep(2000);
-    }
-    await element.fillFilledXpath(`//input[@data-testid='royal_email']`, user);
-    await element.fillFilledXpath(`//input[@data-testid='royal_pass']`, password);
-    await element.clickButtonUntilFindXpath(`//button[@data-testid='royal_login_button']`);
+async function login(user, password) {
+    await element.fillFilledXpath(`//input[@id='email']`, user);
+    await element.fillFilledXpath(`//input[@id='pass']`, password);
+    await element.clickButtonUntilFindXpath(`//button[@name='login']`);
 }
 
 module.exports = {
+    openWebsite: openWebsite,
     login: login,
 }
