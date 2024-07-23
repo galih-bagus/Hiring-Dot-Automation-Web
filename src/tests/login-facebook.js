12d4fc548bfb878ca24028486f319d089bbff5ba
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-07-23 10:49:03 +0700

    fix : change before and scenario

--- src/tests/login-facebook.js
+++ src/tests/login-facebook.js
@@ -6,17 +6,19 @@ const nameValid = process.env.NAME_FACEBOOK_VALID
 describe("Authentication", function() {
     describe("Login", function() {
         before(async function() {
-            let user = process.env.USERNAME_FACEBOOK_VALID
-            let pswd = process.env.PASSWORD_FACEBOOK_VALID
-            await helper.login(user, pswd)
+            await helper.openWebsite()
         })
         after(async function() {
             await driver.sleep(500);
             await driver.quit();
         })
-        it("As user, i can login facebook with valid data", async function() {
-            const nameSidebar = await element.getTextXpath(`(//div[@data-visualcompletion]//span[normalize-space()='${nameValid}'])[2]`)
-            await assert.assertionActualExpected(nameSidebar, nameValid)
+
+        it("As user, i can login facebook with invalid data", async function() {
+            let user = process.env.USERNAME_FACEBOOK_INVALID
+            let pswd = process.env.PASSWORD_FACEBOOK_INVALID
+            await helper.login(user, pswd)
+            const errorMessage = await element.getTextXpath(`//div[@id='error_box']//div[2]`)
+            await assert.assertionActualExpected(errorMessage, 'Invalid username or password')
         })
     })
 })
