160bbec166c9a35e5646ab491193887a141c47d5
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-07-23 11:05:42 +0700

    add : As user, i can't login facebook without password

--- src/tests/login-facebook.js
+++ src/tests/login-facebook.js
@@ -13,10 +13,17 @@ describe("Authentication", function() {
             await driver.quit();
         })
 
-        it("As user, i can login facebook with invalid data", async function() {
+        it("As user, i can't login facebook with invalid data", async function() {
             let user = process.env.USERNAME_FACEBOOK_INVALID
             let pswd = process.env.PASSWORD_FACEBOOK_INVALID
             await helper.login(user, pswd)
+            const errorMessage = await element.getTextXpath(`//div[@id='error_box']//div[2]`)
+            await assert.assertionActualExpected(errorMessage, 'Invalid username or password')
+        })
+        it("As user, i can't login facebook without password", async function() {
+            let user = process.env.USERNAME_FACEBOOK_INVALID
+            let pswd = process.env.PASSWORD_FACEBOOK_INVALID
+            await helper.login(user, pswd, false)
             const errorMessage = await element.getTextXpath(`//div[@id='error_box']//div[2]`)
             await assert.assertionActualExpected(errorMessage, 'Invalid username or password')
         })
